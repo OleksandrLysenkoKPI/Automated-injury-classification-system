cad640a41334af1cae3cc4c68cc42e4320d9121a
OleksandrLysenkoKPI <oleksandrlysenkotb23@gmail.com>
2026-03-31 22:29:53 +0300

    CODE: write function for NumPy file shape examination

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from src.imaging.image_converter import DICOMProcessor
 from src.imaging.utils import verify_npy_conversion
+from src.ml_module.ml_utils import numpy_examinator
 from dotenv import load_dotenv
 import os
 import sys
@@ -9,8 +10,8 @@ if __name__ == "__main__":
     load_dotenv()
     
     dataset_folder = os.getenv('KNEE_CONDITIONS_DATASET')
-    output_png_folder = "data/converted_PNG"
-    output_npy_folder = "data/converted_NumPy"
+    output_png_folder = "data/converted_data/converted_PNG"
+    output_npy_folder = "data/converted_data/converted_NumPy"
     
     test_dicom_image = os.getenv('TEST_DICOM_IMAGE')
     test_numpy_image = os.getenv('TEST_NUMPY_IMAGE')
@@ -19,12 +20,16 @@ if __name__ == "__main__":
     
     print("1 -- Convert DICOM files from dataset")
     print("2 -- Verify DICOM to NumPy file conversion")
+    print("3 -- Examine NumPy file")
     choice_input = int(input())
     
     if choice_input == 1:
         processor.process_all_conditions(dataset_folder, output_png_folder, output_npy_folder)
     elif choice_input == 2:
         verify_npy_conversion(processor=processor, dicom_path=test_dicom_image, npy_path=test_numpy_image)
+    elif choice_input == 3:
+        numpy_examinator('data/prepared_data')
     else:
         print("Exiting program...")
         sys.exit(0)
+    
--- src/ml_module/ml_utils.py
+++ src/ml_module/ml_utils.py
@@ -1,11 +1,38 @@
-import logging
+from ..logger_module.logger import CustomLogger 
 import shutil
 import random
 import os
+from pathlib import Path
+import numpy as np
 
-def log_section(title):
-    """Log text separator"""
-    logging.info(f"{'='*10} {title.upper()} {'='*10}")
+logger = CustomLogger("ML_utils_log")
+
+def numpy_examinator(numpy_folder_root: str | Path):
+    """Prints grouped shapes and relative paths of all .npy files in a folder tree"""
+    shape_map = {}
+    
+    for root, dirs, files in os.walk(numpy_folder_root):
+        for f in files:
+            if f.endswith('.npy'):
+                full_path = os.path.join(root, f)
+                try:
+                    data = np.load(full_path, mmap_mode="r")
+                    shape = data.shape
+                    
+                    rel_path = os.path.relpath(full_path, numpy_folder_root)
+                    
+                    if shape not in shape_map:
+                        shape_map[shape] = []
+                    shape_map[shape].append(rel_path)
+                except Exception as e:
+                    logger.error(f"Could not read {f}: {e}")
+        
+    for shape, paths in shape_map.items():
+        print(f"Shape {list(shape)}")
+        print("Path to files:")
+        for p in paths:
+            print(p)
+        print()
 
 def organize_dataset(source_root, destination_root, train_ratio=0.8):
     conditions_list = [d for d in os.listdir(source_root) if os.path.isdir(os.path.join(source_root, d))]
@@ -41,4 +68,4 @@ def organize_dataset(source_root, destination_root, train_ratio=0.8):
                             shutil.copy2(source_file, destination_file)
                             counter += 1
                             
-        logging.info(f"Class {condition} was processed. Files in Train: {counter-1}")
+        logger.info(f"Class {condition} was processed. Files in Train: {counter-1}")
