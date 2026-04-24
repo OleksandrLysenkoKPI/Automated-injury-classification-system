76b2b28508683246bd634d798de79668bd28cc45
OleksandrLysenkoKPI <oleksandrlysenkotb23@gmail.com>
2026-04-24 18:41:47 +0300

    REFACTOR: adjusted to function changes in other modules

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from src.imaging.image_converter import DICOMProcessor
 from src.imaging.utils import verify_npy_conversion, augment_and_save_dataset, split_data
-from src.ml_module.ml_utils import numpy_examinator
+from src.ml_module.ml_utils import numpy_examiner
 from src.ml_module.data_loader import load_dataset
 from src.ml_module.ml_model import start_model_pipeline
 from dotenv import load_dotenv
@@ -22,28 +22,30 @@ if __name__ == "__main__":
     
     print("1 -- Convert DICOM files from dataset")
     print("2 -- Verify DICOM to NumPy file conversion")
-    print("3 -- Examine NumPy file")
+    print("3 -- Examine NumPy files")
     print("4 -- Split train data")
     print("5 -- Augment NumPy dataset")
     print("6 -- Load dataset")
     print("7 -- Start model pipeline")
     choice_input = int(input())
     
+    # High Fidelity (64, 160, 160), Standard Balanced (48, 224, 224), Deep MRI (96, 128, 128)
+    target_shape = (64, 160, 160)
+    
     if choice_input == 1:
         processor.process_all_conditions(dataset_folder, output_png_folder, output_npy_folder)
     elif choice_input == 2:
         verify_npy_conversion(processor=processor, dicom_path=test_dicom_image, npy_path=test_numpy_image)
     elif choice_input == 3:
-        numpy_examinator('data/prepared_data')
+        numpy_examiner('data/converted_data/converted_NumPy', print_paths=False)
     elif choice_input == 4:
-        split_data('data/prepared_data/train')
+        split_data('data/converted_data/converted_NumPy')
     elif choice_input == 5:
-        augment_and_save_dataset('data/prepared_data/train_split')
+        augment_and_save_dataset('data/prepared_data/train')
     elif choice_input == 6:
-        target_shape = (24, 192, 192) # TODO: Analyze numpy arrays shapes and adjust
-        load_dataset(target_shape, batch_size=4, load_augmented=True)
+        load_dataset(target_shape=target_shape, batch_size=4, load_augmented=True, verify_processing=True, img_idx=10)
     elif choice_input == 7:
-        start_model_pipeline(epochs=50, target_shape=(24, 192, 192), save_file_name="knee_3d_pathology_model", use_augmented=True)
+        start_model_pipeline(epochs=50, target_shape=target_shape, save_file_name="knee_3d_pathology_model", use_augmented=True)
     else:
         print("Exiting program...")
         sys.exit(0)
